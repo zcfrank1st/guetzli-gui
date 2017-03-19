12f94fd5e863714f2b8fae934cf726b5bfa6f160
zcfrank1st <zcfrank1st@sina.com>
2017-03-19 10:58:15 +0800

    add todo

--- src/main/java/com/chaos/guetzli/gui/Action.java
+++ src/main/java/com/chaos/guetzli/gui/Action.java
@@ -39,7 +39,29 @@ public class Action {
 
     @FXML
     private void transform() {
-
+        String pathString = path.getText();
+        if (!pathString.isEmpty()) {
+            File fileOrDir = new File(pathString);
+            if (fileOrDir.isFile()) {
+                // TODO 文件转换
+                System.getProperty("user.dir");
+            } else {
+                File[] files = fileOrDir.listFiles();
+                if (files != null) {
+                    for (File f : files) {
+                        // TODO 文件转换
+                        System.out.println(System.getProperty("user.dir"));
+                    }
+                } else {
+                    // TODO 弹窗转换目录为空
+                }
+            }
+        } else {
+            // TODO 弹窗路径为空
+        }
     }
 
+    private void notify (String message) {}
+
+    private void transformOneFile (String path) {}
 }
