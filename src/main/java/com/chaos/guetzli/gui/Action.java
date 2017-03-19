115789f1e9738d9304f0aa2bcde6ec4f87064371
zcfrank1st <zcfrank1st@sina.com>
2017-03-19 14:07:57 +0800

    done

--- src/main/java/com/chaos/guetzli/gui/Action.java
+++ src/main/java/com/chaos/guetzli/gui/Action.java
@@ -1,19 +1,21 @@
 package com.chaos.guetzli.gui;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
-import javafx.scene.text.TextFlow;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by zcfrank1st on 18/03/2017.
  */
 public class Action {
     @FXML
-    private TextFlow flow;
+    private Label result;
     @FXML
     private Label path;
 
@@ -38,30 +40,59 @@ public class Action {
     }
 
     @FXML
-    private void transform() {
+    private void transform() throws IOException {
         String pathString = path.getText();
         if (!pathString.isEmpty()) {
             File fileOrDir = new File(pathString);
             if (fileOrDir.isFile()) {
-                // TODO 文件转换
-                System.getProperty("user.dir");
+                transformOneFile(fileOrDir);
             } else {
                 File[] files = fileOrDir.listFiles();
                 if (files != null) {
                     for (File f : files) {
-                        // TODO 文件转换
-                        System.out.println(System.getProperty("user.dir"));
+                        transformOneFile(f);
                     }
                 } else {
-                    // TODO 弹窗转换目录为空
+                    makeAlert("ERROR", "需要转换目录为空!");
                 }
             }
         } else {
-            // TODO 弹窗路径为空
+            makeAlert("ERROR", "打开路劲为空!");
         }
     }
 
-    private void notify (String message) {}
+    private void makeAlert (String title, String content) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(content);
 
-    private void transformOneFile (String path) {}
+        alert.showAndWait();
+    }
+
+    private void transformOneFile (File file) throws IOException {
+        ProcessBuilder builder = new ProcessBuilder();
+        List<String> command = new ArrayList<>();
+        command.add(System.getProperty("user.dir") + "/guetzli");
+        command.add(file.getAbsolutePath());
+        command.add(file.getAbsolutePath() + ".compress.jpg");
+        builder.command(command);
+        builder.redirectErrorStream(true);
+        Process p = builder.start();
+
+        output(p.getInputStream());
+
+        // TODO 优化转化展示
+    }
+
+    private void output(InputStream inputStream) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line).append(System.getProperty("line.separator"));
+                result.setText(sb.toString());
+            }
+        }
+    }
 }
